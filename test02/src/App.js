41ff66f179f438dcdc1c5b97ea6b0799d373dae0
Andy-CSKim <andy.cskim@gmail.com>
2023-09-24 21:24:09 +0900

    modify App()

--- test02/src/App.js
+++ test02/src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
 function App() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
 
   return (
     <div className="game">
